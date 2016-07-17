e48ce4330befa3fb9725d0bc361cbe80dd466f57
Nguyen Duc-Duy <ducduy.nguyen@studenti.unitn.it>
2016-07-17 19:52:02 +0000

    ORCA

---
+++ ORCA/meta_io.f90
@@ -0,0 +1,9 @@
+module meta_io
+use parms_com
+implicit none
+save
+    character(len=64) :: opt1,opt2,opt3,opt4,opt5,opt6,opt7,opt8,opt9,opt10,opt11,opt12,opt13,opt14
+    character(len=64) :: svp1,svp2,svp3,svp4,svp5,svp61,svp62,svp63,svp64,svp7,svp8,svp9,svp10,svp11,svp12,svp13,svp14
+    character(len=64), dimension(5) :: blayers,tlayers
+    integer*4 :: consprint
+end module meta_io
